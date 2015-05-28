0270bee3f4b64d4ffa4b42835205e48ed840168f
MiyamotoAkira <yefo.akira@gmail.com>
2015-05-28 09:00:55 +0100

    work done on cancelation of work and rest. Started work on the main DoPomodoros method

--- logic/pomodorologic.py
+++ logic/pomodorologic.py
@@ -11,11 +11,46 @@ class Pomodoro():
 
     def startWork(self):
         self.OnStartOfWork()
-        t = threading.Timer(self.workTime, self.OnEndOfWork)
-        t.start()
+        self.workTimer = threading.Timer(self.workTime, self.OnEndOfWork)
+        self.workTimer.start()
+
+    def startRest(self):
+        self.OnStartOfRest()
+        self.restTimer = threading.Timer(self.restTime, self.OnEndOfRest)
+        self.restTimer.start()
+
+    def cancelWork(self):
+        '''Tries to cancel the work timer. If it wasn't started, it will not fail, and will still raise the events. This behaviour is not guaranteed to remain as it is now. '''
+        self.OnCancelWork()
+        try:
+            self.workTimer.cancel()
+        except:
+            pass
+        self.OnEndOfWork()
+
+    def cancelRest(self):
+        '''Tries to cancel the rest timer. If it wasn't started, it will not fail, and will still raise the events. This behaviour is not guaranteed to remain as it is now. '''
+        self.OnCancelRest()
+        try:
+            self.restTimer.cancel()
+        except:
+            pass
+        self.OnEndOfRest()
 
     def OnStartOfWork(self):
         pub.sendMessage('onStartOfWork')
 
     def OnEndOfWork(self):
         pub.sendMessage('onEndOfWork')
+
+    def OnStartOfRest(self):
+        pub.sendMessage('onStartOfRest')
+
+    def OnEndOfRest(self):
+        pub.sendMessage('onEndOfRest')
+
+    def OnCancelWork(self):
+        pub.sendMessage('onCancelWork')
+
+    def OnCancelRest(self):
+        pub.sendMessage('onCancelRest')
--- tests/test_pomodorologic.py
+++ tests/test_pomodorologic.py
@@ -27,7 +27,7 @@ class TestSetup(unittest.TestCase):
         self.assertEqual(pomodoro.workTime, workTime)
         self.assertEqual(pomodoro.restTime, restTime)
 
-    def test_raise_event_onEndOfWork_over(self):
+    def test_raise_event_onEndOfWork(self):
         workTime = 0.1
         restTime = 5
         pomodoro = pomodorologic.Pomodoro(workTime, restTime)
@@ -48,3 +48,88 @@ class TestSetup(unittest.TestCase):
         self.assertTrue(mocked)
         pub.unsubscribe(mocked, 'onStartOfWork')
 
+    def test_raise_event_onEndOfRest(self):
+        workTime = 0.1
+        restTime = 0.1
+        pomodoro = pomodorologic.Pomodoro(workTime, restTime)
+        mocked = mock. Mock()
+        pub.subscribe(mocked, 'onEndOfRest')
+        pomodoro.startRest()
+        time.sleep(0.2)
+        self.assertTrue(mocked.called)
+        pub.unsubscribe(mocked, 'onEndOfRest')
+
+    def test_raise_event_OnStartOfRest(self):
+        workTime = 0.1
+        restTime = 5
+        pomodoro = pomodorologic.Pomodoro(workTime, restTime)
+        mocked = mock.Mock()
+        pub.subscribe(mocked, 'onStartOfRest')
+        pomodoro.startRest()
+        self.assertTrue(mocked)
+        pub.unsubscribe(mocked, 'onStartOfRest')
+
+    def test_cancel_work(self):
+        workTime = 5
+        restTime = 5
+        pomodoro = pomodorologic.Pomodoro(workTime, restTime)
+        mockCancel = mock.Mock()
+        mockEnd = mock.Mock()
+        pub.subscribe(mockCancel, 'onCancelWork')
+        pub.subscribe(mockEnd, 'onEndOfWork')
+        pomodoro.startWork()
+        pomodoro.cancelWork()
+        self.assertTrue(mockCancel.called)
+        self.assertTrue(mockEnd.called)
+
+    def test_cancel_rest(self):
+        workTime = 5
+        restTime = 5
+        pomodoro = pomodorologic.Pomodoro(workTime, restTime)
+        mockCancel = mock.Mock()
+        mockEnd = mock.Mock()
+        pub.subscribe(mockCancel, 'onCancelRest')
+        pub.subscribe(mockEnd, 'onEndOfRest')
+        pomodoro.startRest()
+        pomodoro.cancelRest()
+        self.assertTrue(mockCancel.called)
+        self.assertTrue(mockEnd.called)
+
+    def test_cancel_work_without_starting(self):
+        workTime = 5
+        restTime = 5
+        pomodoro = pomodorologic.Pomodoro(workTime, restTime)
+        try:
+            pomodoro.cancelWork()
+        except:
+            self.fail("Error was thrown")
+
+    def test_cancel_rest_without_starting(self):
+        workTime = 5
+        restTime = 5
+        pomodoro = pomodorologic.Pomodoro(workTime, restTime)
+        try:
+            pomodoro.cancelRest()
+        except:
+            self.fail("Error was thrown")
+
+    def test_do_a_pomodoro(self):
+        workTime = 0.1
+        restTime = 0.1
+        pomodoro = pomodorologic.Pomodoro(workTime, restTime)
+        mockStartWork = mock.Mock()
+        mockEndWork = mock.Mock()
+        mockStartRest = mock.Mock()
+        mockEndRest = mock.Mock()
+        pub.subscribe(mockStartWork, 'onStartOfWork')
+        pub.subscribe(mockEndWork, 'onEndOfWork')
+        pub.subscribe(mockStartRest, 'onStartOfRest')
+        pub.subscribe(mockEndRest, 'onEndOfRest')
+        pomodoro.DoPomodoros(1)
+        time.sleep(0.3)
+        self.assertTrue(mockStartWork.called)
+        self.assertTrue(mockEndWork.called)
+        self.assertTrue(mockStartRest.called)
+        self.assertTrue(mockEndRest.called)
+        
+        
